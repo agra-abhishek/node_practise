b042821aca22b4a77a8b2e15c2c6daee627f0b8b
Abhishek Agrawal <abhishekagrawal946@gmail.com>
2026-02-12 21:25:26 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,12 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "http://localhost:3000", // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true // if you use cookies or auth
 }));
+
 
 
 
