e6673d772f9d0b9b25923a8d3496e73da5f05c4e
Abhishek Agrawal <abhishekagrawal946@gmail.com>
2026-02-12 20:36:01 +0530

    Updated API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://node-practise-bxq7.onrender.comhttp://localhost:5000/api",
   withCredentials: true, // 🔥 important for cookies
 });
 
