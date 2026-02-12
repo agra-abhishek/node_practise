ffaa72845d603e07481b5a1379426f67b4c1d88d
Abhishek Agrawal <abhishekagrawal946@gmail.com>
2026-02-12 20:42:31 +0530

    Fixed CORS for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,22 @@ const MONGO_URI = process.env.MONGO_URI;
 // MongoDB connection
 connectDB();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://node-practise-1-p0rw.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 
 app.use(express.json());
