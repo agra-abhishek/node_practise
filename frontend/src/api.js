ec6944e906aea370e33d87869f5ac8d95707f46f
Abhishek Agrawal <abhishekagrawal946@gmail.com>
2026-02-12 22:39:14 +0530

    Your commit message

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://node-practise-78.onrender.com/api",
+  baseURL: "https://node-practise-100.onrender.com",
   withCredentials: true, // 🔥 important for cookies
 });
 
